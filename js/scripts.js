9eb413e328da8e10e10d2f2224e137cfbe43fd96
kokocakes <84197710+kokocakes@users.noreply.github.com>
2021-11-08 21:41:58 -0800

    variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
-favoriteDrink ='Vino'
-document.write(favoriteDrink)
+let wineList = [
+    {
+        name: 'Cabernet Sauvgnon',
+        color: 'red',
+        bodyType: ['full']
+    },
+    {
+        name:'Chardonay',
+        color: 'white',
+        bodyType: ['midium to full']
+    },
+    {
+        name: 'Pinot Noir',
+        color: 'red',
+        bodyType:['lighter']
 
-alert("What's your favorite drink?");
+    }
+];
